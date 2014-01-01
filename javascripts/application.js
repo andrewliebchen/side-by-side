0d108a109b34662e319547fa66a34fd3a24915d8
Andrew Liebchen <aliebchen@gofdn.com>
2014-01-01 17:44:46 -0500

    Close pane

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,8 +1,26 @@
 (function() {
-  $(function() {
+  var paneContentHeight;
+
+  paneContentHeight = function() {
     var contentHeight;
     contentHeight = $('.pane-content').outerHeight();
     return $('.pane-tertiary').css('min-height', contentHeight);
+  };
+
+  $(function() {
+    $(document).on('click', '[data-pane-focus]', function(event) {
+      var focusPane;
+      focusPane = $(this).data('pane-focus');
+      event.preventDefault();
+      $('body').addClass("pane-focus-" + focusPane);
+      return paneContentHeight();
+    });
+    return $(document).on('click', '[data-pane-close]', function(event) {
+      var closePane;
+      closePane = $(this).data('pane-close');
+      event.preventDefault();
+      return $('body').removeClass("pane-focus-" + closePane);
+    });
   });
 
 }).call(this);
--- javascripts/application.js.coffee
+++ javascripts/application.js.coffee
@@ -1,3 +1,16 @@
-$ ->
+paneContentHeight = ->
   contentHeight = $('.pane-content').outerHeight()
   $('.pane-tertiary').css('min-height', contentHeight)
+
+$ ->
+
+  $(document).on 'click', '[data-pane-focus]', (event) ->
+    focusPane = $(@).data('pane-focus')
+    event.preventDefault()
+    $('body').addClass("pane-focus-#{focusPane}")
+    paneContentHeight()
+
+  $(document).on 'click', '[data-pane-close]', (event) ->
+    closePane = $(@).data('pane-close')
+    event.preventDefault()
+    $('body').removeClass("pane-focus-#{closePane}")
